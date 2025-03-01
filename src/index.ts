948de136819e12a5c7ce4aa945679decd9f03242
tomi20v <tomi20v@gmail.com>
2025-03-01 21:03:57 +0100

    Rotating iterator (#5)
    * rotating-iterator - added ArrayDimensionIterator
    
    * rotating-iterator - more coverage
    
    * rotating-iterator - ArrayIterator2D now uses the ArrayDimensionIterator
    
    * rotating-iterator - ArrayDimensionIterator renamed to ArrayAxisIterator
    
    * rotating-iterator - add to and uptade readme
    
    * rotating-iterator - ArrayIterator supports array access with proper reverse
    
    * rotating-iterator - extracted IterationMapper interface
    
    * rotating-iterator - readme
    
    * flattening-iterator - refactored mappers
    
    * flattening-iterator - fix typing
    
    * flattening-iterator - bump
    
    * flattening-iterator - add coverage to .gitignore
    
    * flattening-iterator - update exports
    
    * flattening-iterator - bump
    
    * bump, fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { default as ArrayIterator } from "./ArrayIterator";
 export { default as ArrayIterator2D } from "./ArrayIterator2D";
+export { default as ArrayAxisIterator } from "./ArrayAxisIterator";
+export { default as FlatteningIterator } from "./FlatteningIterator";
+export { type IterationItem } from "./IterationItem";
+export { type IterationMapper } from "./IterationMapper";
+export { move, multiply, onlyTheValue, rotateCoords, scale } from "./IteratorMappers";
